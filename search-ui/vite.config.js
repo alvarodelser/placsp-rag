5797232fa08620e870992ea72cbf00b9631cb4e0
AlvarodelSer <a.fontecha.ser@gmail.com>
2026-06-23 00:50:38 +0200

    Modified npm build

--- search-ui/vite.config.js
+++ search-ui/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// VITE_API_BASE points the UI at the search-api backend (see .env.example).
 export default defineConfig({
   plugins: [react()],
+  base: '/placsprag/',
   server: { port: 5173 },
 })
